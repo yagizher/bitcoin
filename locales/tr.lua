55d76f3caa5f3d0e00aff84880f4718be28393e8
BuildTools <unconfigured@null.spigotmc.org>
2019-09-18 14:15:11 +0300

    TR Bitcoin

---
+++ locales/tr.lua
@@ -0,0 +1,10 @@
+Locales['tr'] = {
+	['carrega_para_apanhares_frutos'] = 'Bitcoin kazmak için ~INPUT_CONTEXT~ tuşuna bas.',
+	['carrega_para_venderes_os_sacos'] = 'Bitcoin satmak için ~INPUT_CONTEXT~ tuşuna bas.',
+	['mochila_full'] = 'Daha fazla Bitcoin toplayamazsın çünkü envanterin dolu!',
+	['pegar_frutos'] = '~y~Bitcoin toplanıyor~s~...',
+	['nao_tens_mais_frutos'] = 'Daha fazla ~r~Bicoin~s~e sahip değilsin',
+	['nao_tens_sacos_com_frutos'] = 'Daha fazla ~r~Bitcoin~s~e sahip değilsin',
+	['vendeste_sacos'] = '~y~x1~s~ adet ~y~Bitcoin~s~ sat',
+	['venda_do_sacos'] = '~g~Bitcoin satış~s~...',
+}
